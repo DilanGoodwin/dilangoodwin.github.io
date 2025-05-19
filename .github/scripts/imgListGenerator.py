89a39916a4da03a5d4692d3f397a1b006bb13532
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-05-19 18:26:47 +0100

    First attempt auto JSON generation

---
+++ .github/scripts/imgListGenerator.py
@@ -0,0 +1,17 @@
+import os
+import json
+
+def pullFiles(path):
+    file_list = []
+    for _, _, files in os.walk(path):
+        for file in files:
+            if file.endswith(('.png', '.jpg', '.jpeg', '.gif')):
+                file_list.append(file)
+    return file_list
+
+retrievedFileList=pullFiles('img/gallery/')
+
+# Output to a specific location
+output_path = 'img/gallery/GalleryList.json'
+with open(output_path, 'w') as f:
+    json.dump(retrievedFileList, f, indent=2)
