d187df4ba58de16c86e25b5420e6529d809742f8
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-05-20 11:07:14 +0100

    Fixed Masonry issues on Gallery page

--- js/Gallery.js
+++ js/Gallery.js
@@ -11,6 +11,40 @@ async function fetchImagesFromFolder(folderPath) {
     return images.map(img => `${folderPath}/${img}`);
 }
 
+// Initialize Masonry after images are loaded
+function applyMasonry(containerId) {
+    const container = document.getElementById(containerId);
+    if (!container) return;
+    
+    const images = container.getElementsByTagName('img');
+
+    let loadedCount = 0;
+    const total = images.length;
+    if (total === 0) return;
+
+    for (let img of images) {
+        if (img.complete) {
+            loadedCount++;
+            if (loadedCount === total) {
+                new Masonry(container, {
+                    itemSelector: '.col-sm-6.col-md-4.mb-4',
+                    percentPosition: true
+                });
+            }
+        } else {
+            img.onload = () => {
+                loadedCount++;
+                if (loadedCount === total) {
+                    new Masonry(container, {
+                        itemSelector: '.col-sm-6.col-md-4.mb-4',
+                        percentPosition: true
+                    });
+                }
+            };
+        }
+    }
+}
+
 // Generate the image tiles and add them to the container
 // Each image is within its own div allowing for Masonry to lay them out as necessary
 async function displayGallery(folderPath, containerId) {
@@ -23,11 +57,12 @@ async function displayGallery(folderPath, containerId) {
             wrapper.className = 'col-sm-6 col-md-4 mb-4';
             const img = document.createElement('img');
             img.src = url;
-            img.className='img-fluid rounded shadow-sm';
+            img.className = 'img-fluid rounded shadow-sm';
             wrapper.appendChild(img);
             container.appendChild(wrapper);
         });
-    } catch (err){
+        applyMasonry(containerId);
+    } catch (err) {
         // If there is an error don't bother doing anything as we are likely running on a users machine
         // For development add log lines here for errors
         // console.log(err)
