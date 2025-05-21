64a78b9913e1447dad3c5046a6f1d38aaaa68557
Dilan Goodwin <dilanjgoodwin@yahoo.co.uk>
2025-05-21 07:18:21 +0100

    Updated auto generate image script for JPG images

--- .github/scripts/imgListGenerator.py
+++ .github/scripts/imgListGenerator.py
@@ -5,11 +5,12 @@ def pullFiles(path):
     file_list = []
     for _, _, files in os.walk(path):
         for file in files:
-            if file.endswith(('.png', '.jpg', '.jpeg', '.gif')):
+            if file.endswith(('.png', '.jpg', '.JPG', '.jpeg', '.gif')):
                 file_list.append(file)
     return file_list
 
 retrievedFileList=pullFiles('img/gallery/')
+print(retrievedFileList)
 
 # Output to a specific location
 output_path = 'img/gallery/GalleryList.json'
